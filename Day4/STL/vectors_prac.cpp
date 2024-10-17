df2bc1586566783dada5cf00c3bd84867250a547
Kirti Arora <Kirti-1@users.noreply.github.com>
2024-10-18 00:37:06 +0530

    makePair

--- Day4/STL/vectors_prac.cpp
+++ Day4/STL/vectors_prac.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+
+void makePair(int arr[], vector<pair<int, int> > &out, int i, int j, int n, int& countOp) {
+    if (j == n) {  // Terminating condition for j
+        if (i < n - 1) {  // Move to the next i
+            i = i + 1;
+            j = i + 1;
+        } else {
+            return;
+        }
+    }
+
+    if (j < n && i < n-1) {  // Only push if j is within bounds
+        out.push_back(pair<int, int>(arr[i], arr[j]));
+        makePair(arr, out, i, j + 1, n, countOp);  
+        countOp++;
+    // Increment j
+    }
+
+}
 int main(){
 
 
@@ -46,7 +65,18 @@ int main(){
     cout << "Is the queue empty after clearing it? " << (vec.empty() == 1 ? " Yes ": " No ") << endl;
 
 
+    int arr[6] = {2, 3, 5, 6, 7, 9};
+    vector<pair<int, int> > out;
+    int count = 0; 
+    makePair(arr, out, 0, 1, 6, count);
 
 
+    // Printing the pairs
+    for (size_t i=0;i<out.size();i++) {
+        cout << "(" << out[i].first << ", " << out[i].second << ") ";
+    }
+
+
+    cout<< "\n" << count;
     return 0;
 }
